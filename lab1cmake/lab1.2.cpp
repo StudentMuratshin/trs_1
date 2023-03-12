3fcf37ebbc6da9acc63a1a77a477396f63c4779e
Ильuам Муратшин <PETA4OK96@mail.ru>
2023-03-12 08:27:23 +0500

    check

---
+++ lab1cmake/lab1.2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+double RHS_v(double t, double m)
+{
+    return -1 / m * (2 * t + 2 * t * tan(t * t) * tan(t * t));
+}
+
+double RHS_x(double t)
+{
+    return t;
+}
+
+
+int main()
+{
+    ofstream X_2("X2_out.csv");
+    ofstream V_2("V2_out.csv");
+    double x_2, x_n = x_2 = 0;
+    double v_2, v_n = v_2 = 1;
+    double m = 1.67;
+    vector<int> n = { 10001, 1001, 101, 11 };
+    for (int j = 0; j < n.size(); j++)
+    {
+        double h = 10. / n[j];
+        double x_1 = x_2 + h * RHS_x(v_2);
+        double v_1 = v_2 + h * RHS_v(x_2, m);
+        for (int i = 0; i < n[j]; i++)
+        {
+            X_2 << i * h << " " << x_n << endl;
+            V_2 << i * h << " " << v_n << endl;
+            x_n = x_1 + h / 2. * (3 * RHS_x(v_1) - RHS_x(v_2));
+            v_n = v_1 + h / 2. * (3 * RHS_v(x_1, m) - RHS_v(x_2, m));
+            x_2 = x_1;
+            v_2 = v_1;
+            x_1 = x_n;
+            v_1 = v_n;
+            //cout << i * h << ' ' << x_1 << endl;
+        }
+    }
+    X_2.close();
+    V_2.close();
+        
+    //Задание 2
+
+}
---
+++ lab1cmake/lab1cmake.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+double dv_dt(double t, double m)
+{
+    return -1 / m * (2 * t + 2 * t * tan(t * t) * tan(t * t));
+}
+
+double dx_dt(double t)
+{
+    return t;
+}
+
+
+int main()
+{
+    ofstream X("X_out.csv");
+    ofstream V("V_out.csv");
+    double x_n_1, x_n = x_n_1 = 0;
+    double v_n_1, v_n = v_n_1 = 1;
+    double m = 1.67;
+    int n = 10001;
+    double h = 10. / n;
+    for (int i = 0; i < n; i++)
+    {
+        X << i * h << " " << x_n << endl;
+        V << i * h << " " << v_n << endl;
+        x_n = x_n_1 + h * dx_dt(v_n_1);
+        v_n = v_n_1 + h * dv_dt(x_n_1, m);
+        x_n_1 = x_n;
+        v_n_1 = v_n;
+        cout << i * h << ' ' << x_n_1 << endl;
+    }
+    X.close();
+    V.close();
+    //Задание 2
+
+}
